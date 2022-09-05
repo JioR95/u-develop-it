c36f400fe58e68c3325b9e09588e7087f85ad863
JioR95 <jiovani.rivera@rocketmail.com>
2022-09-05 16:23:16 -0400

    add CRUD routes

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -1,5 +1,4 @@
-// utility to check if an object has the required properties
-// e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
+tCheck(object, 'prop1', 'prop2', 'etc')
 
 module.exports = function(obj, ...props) {
     const errors = [];
